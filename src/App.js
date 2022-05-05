7a4c8103cd53fb9b05641ccbb3b0478ad0bc1811
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-05-04 19:00:01 -0700

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [text, setText] = useState("Welcome!");
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setText(""), 2000);
+  })
+
+  return (
+  <React.Fragment>
+    <button>Click Me</button>
+    <input placeholder="Write your own website title" onChange={(event) => setText(event.target.value)}></input>
+    <DogPics />
+  </React.Fragment>
+  );
 }
 
 export default App;
